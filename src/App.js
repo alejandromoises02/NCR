f5cbfde2a653da91fa9db16126d6b3ec14c36386
alejandromoises <alejandromoises02@gmail.com>
2021-01-29 18:52:50 -0300

    First Commit, consumiendo API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {CuentasProvider} from './context/Cuentas';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <CuentasProvider>
+          <h1>HOLA MUNDO</h1>
+        </CuentasProvider>
+      </main>
     </div>
   );
 }
